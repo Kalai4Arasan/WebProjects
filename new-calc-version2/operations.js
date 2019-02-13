11731669a0aac89ff89ca94f2fa75504193e069c
Kalai4Arasan <47599995+Kalai4Arasan@users.noreply.github.com>
2019-02-13 21:10:13 +0530

    Add files via upload

---
+++ new-calc-version2/operations.js
@@ -0,0 +1,50 @@
+function add(){
+    var number1=document.querySelector('#number1').value;
+    var number2=document.querySelector('#number2').value;
+    document.querySelector("#result").value=parseInt(number1)+parseInt(number2);
+}
+function sub(){
+    var number1=document.querySelector('#number1').value;
+    var number2=document.querySelector('#number2').value;
+    document.querySelector("#result").value=parseInt(number1)-parseInt(number2);
+}
+function mul(){
+    var number1=document.querySelector('#number1').value;
+    var number2=document.querySelector('#number2').value;
+    document.querySelector("#result").value=parseInt(number1)*parseInt(number2);
+}
+function div(){
+    var number1=document.querySelector('#number1').value;
+    var number2=document.querySelector('#number2').value;
+    document.querySelector("#result").value=parseInt(number1)/parseInt(number2);
+}
+function mod(){
+    var number1=document.querySelector('#number1').value;
+    var number2=document.querySelector('#number2').value;
+    document.querySelector("#result").value=parseInt(number1)%parseInt(number2);
+}
+function addsub()
+{
+add();
+sub();
+}
+function clear1()
+{
+ document.querySelector("#number1").value=" ";
+ document.querySelector("#number2").value=" ";
+ document.querySelector("#result").value=0;
+}
+function clear2()
+{
+    document.querySelector("#number1").value=" "; 
+}
+
+function get(val)
+{
+    document.getElementById("number1").value+=val;
+}
+function swap()
+{
+   document.getElementById("number2").value=document.getElementById("number1").value;
+   clear2();
+}
